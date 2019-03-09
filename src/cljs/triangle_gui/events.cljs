1addd1da4757d3c28d69c66b3e9058542a37429e
solenyaPickleman <bradyadice@gmail.com>
2019-03-09 21:32:09 +0200

    clicking now resets the board and sets the empty space to the clicked peg

--- src/cljs/triangle_gui/events.cljs
+++ src/cljs/triangle_gui/events.cljs
@@ -24,13 +24,26 @@
   handle-next-turn
   )
 
+;this section builds a  game
+(defn get-peg "gets a peg or space" [is-peg]
+  (if is-peg 0 1))
+
+(defn split-tree "takes a list and builds it into the game board format"
+  [ l]
+  [(subvec l 0 1)
+   (subvec l 1 3)
+   (subvec l 3 6)
+   (subvec l 6 10)
+   (subvec l 10 15)
+   ]
+  )
 
 (defn set-peg
   [coeffects event]
   (let [i (second event)                    ;; extract id from event vector
         db (:db coeffects)]               ;; extract the current application state
-    (js/console.log i )
-    {:db db}
+    (js/console.log i (split-tree (into [] (map #(get-peg (= % i)) (range 15)))))
+    {:db (assoc db :game (split-tree (into [] (map #(get-peg (= % i)) (range 15)))))}
     ))
 
 (re-frame/reg-event-fx
